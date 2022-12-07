dcbedaaaf348da116f925414dd7daeed93d0df3a
CallMeRyax <117657481+CallMeRyax@users.noreply.github.com>
2022-12-07 13:37:08 +0100

    Update skripta.sh

--- skripta.sh
+++ skripta.sh
@@ -32,3 +32,4 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get -y update
 apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+echo KONEC SKRIPTE
